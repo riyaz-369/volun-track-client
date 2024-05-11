c5b802dbbf161b96f8ff3f0a1f2901c8084a20e2
Your Nameriyazulislam99 <riyazulislam0099@gmail.com>
2024-05-11 15:09:26 +0600

    delete my volunteer post using specific id

--- src/pages/MyPost/MyPost.jsx
+++ src/pages/MyPost/MyPost.jsx
@@ -1,25 +1,64 @@
 import { useEffect, useState } from "react";
-import useAxiosCommon from "../../hooks/useAxiosCommon";
 import useAuth from "../../hooks/useAuth";
 import { Link } from "react-router-dom";
 import { FiEdit } from "react-icons/fi";
 import { RiDeleteBin6Line } from "react-icons/ri";
+import toast from "react-hot-toast";
+import useAxiosCommon from "../../hooks/useAxiosCommon";
+import Swal from "sweetalert2";
 
 const MyPost = () => {
   const { user } = useAuth();
   const axiosCommon = useAxiosCommon();
   const [myPosts, setMyPosts] = useState([]);
-  console.log(myPosts);
 
   useEffect(() => {
-    const getData = async () => {
-      const { data } = await axiosCommon(
-        `/volunteers-email?email=${user?.email}`
-      );
-      setMyPosts(data);
-    };
     getData();
   }, [user]);
+
+  const getData = async () => {
+    const { data } = await axiosCommon(
+      `/volunteers-email?email=${user?.email}`
+    );
+    setMyPosts(data);
+  };
+
+  const handleDelete = (id) => {
+    try {
+      Swal.fire({
+        title: "Are you sure?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#748E54",
+        cancelButtonColor: "#553739",
+        confirmButtonText: "Yes",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          axiosCommon
+            .delete(`/volunteers/${id}`)
+            .then((data) => {
+              console.log(data);
+              if (data.deletedCount > 0) {
+                Swal.fire({
+                  title: "Deleted!",
+                  text: "Your volunteer post deleted.",
+                  icon: "success",
+                });
+                getData();
+              }
+            })
+            .catch(() => {
+              Swal.fire({
+                title: "Something went wrong !",
+                icon: "warning",
+              });
+            });
+        }
+      });
+    } catch (err) {
+      toast.error(err.message);
+    }
+  };
 
   return (
     <div className="overflow-x-auto max-w-7xl mx-auto my-12">
@@ -43,12 +82,18 @@ const MyPost = () => {
               <th>{idx + 1}</th>
               <td>{myPost.post_title}</td>
               <td>{myPost.category}</td>
-              <td>{myPost.deadline}</td>
+              <td>{new Date(myPost.deadline).toLocaleDateString()}</td>
               <td className="flex gap-4 text-xl">
-                <Link className="hover:text-[#8cbd51]">
+                <Link
+                  to={`/update/${myPost._id}`}
+                  className="hover:text-[#8cbd51]"
+                >
                   <FiEdit />
                 </Link>
-                <button className="hover:text-[#a86340]">
+                <button
+                  onClick={() => handleDelete(myPost._id)}
+                  className="hover:text-[#a86340]"
+                >
                   <RiDeleteBin6Line />
                 </button>
               </td>
---
+++ src/pages/MyPost/UpdatePost.jsx
@@ -0,0 +1,9 @@
+const UpdatePost = () => {
+  return (
+    <div>
+      <h1>update post</h1>
+    </div>
+  );
+};
+
+export default UpdatePost;
